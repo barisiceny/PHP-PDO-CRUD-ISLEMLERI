48475a282438e86cf382f37923d89926d2a1cdb4
Barış İçen <icenbaris50@gmail.com>
2025-07-08 14:12:07 -0400

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,17 +1,17 @@
 <?php
 if ($_GET) {
 
-    // $page = $_GET["page name"];     If you have defined the page name for your admin panel, you can use this
-    include("fonc.php"); // we include our database connection on our page.
+    // $page = $_GET["sayfa adı"]; Yönetici paneliniz için sayfa adını tanımladıysanız, bunu kullanabilirsiniz
+    include("fonc.php"); // Veritabanı bağlantımızı sayfamıza dahil ediyoruz.
     $query = $connect->prepare("SELECT * FROM article Where id=:id");
     $query->execute(['id' => (int)$_GET["id"]]);
-    $result = $query->fetch();//executing query and getting data
+    $result = $query->fetch();//Sorguyu yürütme ve veri alma
     
-        // We write our query code to delete the data whose id is selected.
+        // Kimliği seçilen verileri silmek için sorgu kodumuzu yazıyoruz.
     $where = ['id' => (int)$_GET['id']];
     $status = $connect->prepare("DELETE FROM article WHERE id=:id")->execute($where);
     if ($status) {
-        header("location:index.php"); // If the query runs, we send it to the index.php page.
+        header("location:index.php"); // Sorgu çalışırsa, index.php sayfasına göndeririz.
     }
 }
 ?>
